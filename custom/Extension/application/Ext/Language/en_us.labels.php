126a921efa95c417c1c63bd61c21946b900fca3d
Roniel Bernas <roniel.bernas@crmworksa.asia>
2017-05-27 01:22:32 +0800

    basic dashlet files + labels (english)

---
+++ custom/Extension/application/Ext/Language/en_us.labels.php
@@ -0,0 +1,3 @@
+<?php
+$app_strings['LBL_DASHLET_DROPBOX_LABEL'] = 'Your Dropbox Files';
+$app_strings['LBL_DASHLET_DROPBOX_DESCRIPTION'] = 'Dashlet that integrates with Dropbox API';
---
+++ custom/clients/base/views/dashlet-dropbox/dashlet-dropbox.php
@@ -0,0 +1,12 @@
+<?php
+$viewdefs['base']['view']['dashlet-dropbox'] = array(
+    'dashlets' => array(
+        array(
+            'label' => 'LBL_DASHLET_DROPBOX_LABEL',
+            'description' => 'LBL_DASHLET_DROPBOX_DESCRIPTION',
+            'config' => array(),
+            'preview' => array(),
+            'filter' => array()
+        ),
+    ),
+);
